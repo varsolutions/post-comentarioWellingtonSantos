8919db5e94de9731610b84522e3212946b293765
willingtonsilvasantos <wellington.eng82@gmail.com>
2023-08-21 09:17:12 -0300

    subindo os restante dos arquivos

--- js/index.js
+++ js/index.js
@@ -8,13 +8,29 @@ const urlParametros = new URLSearchParams(window.location.search)
 const idPost = urlParametros.get("id")
 const comentariosContainer = document.querySelector("#comentarios-container")
 
+const comentarioForm = document.querySelector("#comentario-form")
+const emailinput = document.querySelector("#email")
+const comentarioinput = document.querySelector("#tcomentario")
+
 if (!idPost) {
     BuscarTodosPosts()
 }
 else {
-    // Tratar aqui o metodo de gravar comentários e visualizar detalhe dp post
-    console.log("o valor do idPost é" + idPost)
-    BuscarPostEspecifico(idPost)
+        BuscarPostEspecifico(idPost)
+
+        comentarioForm.addEventListener("submit",(e) =>{
+            e.preventDefault()
+            let comentarioInserido = {
+                email: emailinput.value,
+                body: comentarioinput.value,
+           
+            }
+
+comentarioInserido = JSON.stringify(comentarioInserido)
+
+postComentario(comentarioInserido)
+        })
+
 }
 
 async function BuscarTodosPosts() {
@@ -47,13 +63,19 @@ async function BuscarTodosPosts() {
 }
 
 async function BuscarPostEspecifico(id) {
-    const respostaPost = await fetch(`${url}/${id}`)
-    const respostaComentario = await fetch(`${url}/${id}/comments`)
+    //const respostaPost = await fetch(`${url}/${id}`)
+   // const respostaComentario = await fetch(`${url}/${id}/comments`)
+
+const[respostaPost, respostaComentario] = await Promise.all([
+    fetch(`${url}/${id}`),
+    fetch(`${url}/${id}/comments`),
+
+])
 
     const dataPostagem = await respostaPost.json()
     const datacomentario = await respostaComentario.json()
 
-    console.log(dataPostagem)
+    loadingElement.classList.add("hide")
 
     const title = document.createElement("h1")
     const body = document.createElement("p")
@@ -81,5 +103,17 @@ paragrafocomentario.innerText = comentario.body
 divComentario.appendChild(email)
 divComentario.appendChild(paragrafocomentario)
 comentariosContainer.appendChild(divComentario)
+}
 
+async function postComentario (comentario)
+{
+    const resposta = await fetch(url, {
+        method: "POST",
+        body: comentario,
+        headers:{
+            "Content-type": "application/json",
+        }
+      })
+      const dataResposta = await resposta.json()
+      criarComentario(dataResposta)
 }
